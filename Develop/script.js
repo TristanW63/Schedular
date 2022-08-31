84934fd5bb3dbba898cacd64afa7345119a6dfd6
Tristan <tristan_wilson_63@yahoo.com>
2022-08-31 14:36:38 -0500

    rewrote code in different way to solve bug

--- Develop/script.js
+++ Develop/script.js
@@ -1,163 +1,51 @@
-const workDay = [
-    {
-        id: "0",
-        hour: "09",
-        time: "09",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "1",
-        hour: "10",
-        time: "10",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "2",
-        hour: "11",
-        time: "11",
-        meridiem: "am",
-        reminder: ""
-    },
-    {
-        id: "3",
-        hour: "12",
-        time: "12",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "4",
-        hour: "01",
-        time: "13",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "5",
-        hour: "02",
-        time: "14",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "6",
-        hour: "11",
-        time: "23",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "7",
-        hour: "04",
-        time: "16",
-        meridiem: "pm",
-        reminder: ""
-    },
-    {
-        id: "8",
-        hour: "05",
-        time: "17",
-        meridiem: "pm",
-        reminder: ""
-    },
-]
 
 //gets date
 function getHeaderDate() {
     var currentHeaderDate = moment().format('dddd, MMMM Do');
     $("#currentDay").text(currentHeaderDate);
 }
+var momentTime = moment().startOf("day").add(7, "hour");
+var hour = moment().format("HH");
 
-//saves info to local storage
-// function saveInfo() {
-//     localStorage.setItem("workDay", JSON.stringify(workDay));
-// }
+for(var i = 8; i < 18; i++) {
+    var timeSlot = momentTime.add(1, "hour").format("HH:mm A");
+    var currentState;
 
-// function displayInfo() {
-//     workDay.forEach(function (_thisHour) {
-//         $(`#${_thisHour.id}`).val(_thisHour.reminder);
-//     })
-// }
+    if (hour == i) {
+        currentState = 'present';
+    } else if (hour < i) {
+        currentState = 'future';
+    } else if (hour > i) {
+        currentState = 'past';
+    }
 
-// function init() {
-//     var storedDay = JSON.parse(localStorage.getItem("workDay"));
+    var newStuff =
 
-//     if (storedDay) {
-//        let workDay = storedDay;
-//     }
+    `<container class="row" id='hour-${i}'>
+            <div class="col-2"></div>
+            <div class="hour w-25 p-4 col-1">${timeSlot}</div>
+            <textarea class="description w-50 p-4 col-6 ${currentState} hour-${i}"></textarea>
+            <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
+            <div class="col-2">
+            </div>  
+        </container>
+        <p></p>`
+        ;
 
-//     saveInfo();
-//     displayInfo();
-// }
+    $(".container").append(newStuff);
+
+};
+
 
 getHeaderDate();
-//creates rows for time slots
-workDay.forEach(function(thisHour) {
-
-    var timeSlotRows = $("<form>").attr({
-        "class": "row"
-    });
-    $(".container").append(timeSlotRows);
-
-    var displayHours = $("<div>")
-    .text(`${thisHour.hour}${thisHour.meridiem}`)
-    .attr({
-        "class": "col-md-2 hour"
-    });
-
-    var hourPlan = $("<div>")
-    .attr({
-        "class": "col-md-9 description p-0"
-    });
-
-//sets past, present or futre classes
-    var classesData = $("<textarea>");
-displayHours.append(classesData);
-classesData.attr("id", thisHour.id);
-if (thisHour.time < moment().format("HH")) {
-    classesData.attr ({
-        "class": "past"
-    })
-    
-} else if (thisHour.time === moment().format("HH")) {
-    classesData.attr ({
-        "class": "present"
-    })
-} else if (thisHour.time > moment().format("HH")) {
-    classesData.attr ({
-        "class": "future"
-    })
-}
-
-//save button
-var saveButton = $("<i class='far fa-save fa-lg'></i>")
-var savePlan = $("<button>")
-.attr({
-    "class": "col-md-1 saveBtn"
-});
-savePlan.append(saveButton);
-timeSlotRows.append(displayHours, hourPlan, savePlan);
-})
-
-// init();
-
-// $(".saveBtn").on("click", function(event) {
-//     event.preventDefault();
-//     var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-//     workDay.saveIndex.reminder = $(this).siblings(".description").children(".future").val();
-//     console.log(saveIndex);
-//     saveInfo();
-//     displayInfo();
-// })
-
-$(document).ready(function () {
 
    $(".saveBtn").on("click", function () {
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, text);
-   })
-})
+   });
+
+   for (var i = 8; i < 18; i++) {
+    $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
+}
